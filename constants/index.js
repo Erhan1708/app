667e6d704889aa992e39f8cf1bf9860460ddffe2
Erhan <erhankubanych@gmail.com>
2022-11-11 14:48:21 +0600

    added constants

---
+++ constants/index.js
@@ -0,0 +1,23 @@
+export const navBar = [
+   {
+      id: 1,
+      title: 'Home',
+      path: '/'
+   },
+   {
+      id: 2,
+      title: 'All products',
+      path: '/products'
+   },
+   {
+      id: 3,
+      title: 'Support',
+      path: '/support'
+   }
+]
+
+export const footerNavBar = [
+   {
+      
+   }
+]

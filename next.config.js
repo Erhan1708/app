2e65fe2caa444989e8ad69ed33419e011d459dd8
Erhan <erhankubanych@gmail.com>
2022-11-15 15:44:32 +0600

    added render image through links

--- next.config.js
+++ next.config.js
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+   reactStrictMode: true,
+   images: {
+      domains: [
+         'test-docs.stores.kg',
+         'docs.stores.kg',
+         'test-docs.stores.kg/images',
+         'docs.stores.kg/images',
+         'test-docs.stores.kg/images/product_images',
+         'docs.stores.kg/images/product_images',
+         'localhost',
+         'web.imd.kg',
+         'static-s.aa-cdn.net'
+      ],
+   },
+   // images: {
+   //    remotePatterns: [
+   //       {
+   //          protocol: 'https',
+   //          hostname: 'test-docs.stores.kg',
+   //          pathname: '/images/product_images',
+   //       },
+   //    ],
+   // },
 }
 
 module.exports = nextConfig
